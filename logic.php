d7a23c1c2da2bc99c256fbecf851e50a46443858
Mindespa <mindaugas.strong@gmail.com>
2023-03-23 20:07:19 +0200

    sutvarkytas texto ivedimas

--- logic.php
+++ logic.php
@@ -2,16 +2,14 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
- 
-   if ( $_POST['Item'] != "" ) {
+   
+   if (strlen( preg_replace("/[^a-z]/i","", $_POST['Item'])) >= 3 && strlen( preg_replace("/[^a-z]/i","", $_POST['Category'])) >= 3 ) {
     $_SESSION['shop'][] = ['item' => $_POST['Item'] , 'category' => $_POST['Category']];
    }
-
+  header ("Location: ./index.php");
+  die;
 }
 
-print_r($_SESSION['shop']); // jei sita eilute meta erora del sitos eilutes - nekriepk demesio ir idek preke. dings
+// print_r($_SESSION['shop']); // jei sita eilute meta erora del sitos eilutes - nekriepk demesio ir idek preke. dings
 
-// $shop = array (
-//     array("obuoliai",100),
-    
-//   );
+
